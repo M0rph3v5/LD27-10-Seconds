623d5947419921e7b6ccdd28cbb0fe6e683a6291
Benjamin de Jager <m0rph3v5@gmail.com>
2013-08-24 16:53:22 +0200

    hotween editor stuff, not sure if gonna use.

---
+++ Assets/Scripts/StairScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StairScript : RewindableScript {
+	
+	List<GameObject> stairPieces;
+	
+	void Start () {
+		stairPieces = new List<GameObject>();
+		
+		for(int i = 0; i < transform.childCount; i++) {
+			stairPieces.Add((GameObject)transform.GetChild(i).gameObject);
+		}
+		
+			
+	}
+	
+	void Update () {
+		
+	}
+	
+	public override void WillStopRewinding () 	{
+		base.WillStopRewinding ();
+		
+	}
+	
+	public override void WillStartRewinding () 	{
+		base.WillStartRewinding ();
+		
+	}
+}
