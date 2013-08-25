a663f9fd1eaed63c28d6171b2b4537e74130cb79
Benjamin de Jager <m0rph3v5@gmail.com>
2013-08-25 16:39:46 +0200

    allemala meuk

---
+++ Assets/GUIScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		OVRCrosshair crosshair = new OVRCrosshair();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI () {
+		
+		
+		
+	}
+}
