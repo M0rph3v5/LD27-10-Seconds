e1020ae5514443fec69a1cd34ec20d5cd73b3f52
Rahul Choudhury <rahul@q42.nl>
2013-08-25 20:09:35 +0200

    Add gameover screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,15 +11,30 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		gameObj = gameObject;
+		Object.DontDestroyOnLoad(gameObj);
 		InvokeRepeating("checkTimer", 1.0f, 1.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!gameOver) return;
+		
+		if (Application.loadedLevelName == "Game") {
+			Application.LoadLevel("Gameover");
+		}
+		
+		if (Application.loadedLevelName == "Gameover") {
+			if (Input.GetKey (KeyCode.Escape)) {
+				Application.Quit();
+			}
+			else if (Input.anyKey) {
+				Application.LoadLevel("Game");
+			}
+		}
 	}
 	
 	void checkTimer() {
-		if (timer > 0) {
+		if (!gameOver && timer > 0) {
 			timer--;
 			if (timer == 0) {
 				gameOver = true;
@@ -34,10 +49,6 @@ public class GameManager : MonoBehaviour {
 	
 	void OnGUI() {
 		GUI.Label(new Rect(10, 10, 200, 50), score.ToString());
-		
 		GUI.Label (new Rect(500, 10, 200, 50), timer.ToString());
-		
-		if (gameOver)
-			GUI.Label (new Rect(200, 100, 200, 50), "game over!!!");
 	}
 }
